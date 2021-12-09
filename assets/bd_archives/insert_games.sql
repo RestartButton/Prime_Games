c327f5cf107ad7a501011a2d7152c166ce2d7532
Leonardo Borck <l.borck@edu.univali.br>
2021-12-08 21:44:18 -0300

    correction
    name of desenvolvedora is wrong

--- assets/bd_archives/insert_games.sql
+++ assets/bd_archives/insert_games.sql
@@ -3,7 +3,7 @@ use primegames;
 INSERT INTO jogo(nome,genero,desenvolvedora,status) VALUES
 	("The Last Of Us Part II","Acao/Aventura","Naughty Dog","Disponivel"),
     ("God Of War","Acao/Aventura","Santa Monica Studio","Disponivel"),
-    ("Fifa 22","Futebol Eletronico","EA GAMES","Disponivel"),
+    ("Fifa 22","Futebol Eletronico","Electronic Arts","Disponivel"),
     ("Spider-Man","Acao/Aventura","Insomniac Games","Disponivel"),
     ("Grand Teft Auto V","Mundo Aberto","Rockstar Games","Disponivel"),
     ("Far Cry 6","Tiro em Primeira Pessoa","Ubisoft","Disponivel"),
